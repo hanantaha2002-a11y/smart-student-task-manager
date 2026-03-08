d9563e2baa9172be8da393122ca5a992dcc13c17
hanantaha2002-a11y <hanantaha.2002@gmail.com>
2026-03-08 19:02:15 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function addTask() {
+    let taskInput = document.getElementById("taskInput");
+    let taskText = taskInput.value;
+
+    if(taskText === "") return;
+
+    let li = document.createElement("li");
+    li.textContent = taskText;
+
+    document.getElementById("taskList").appendChild(li);
+
+    taskInput.value = "";
+}
